669ed2afab809105a139232774d6d9eb14261e40
tszajko <tszajko@gmail.com>
2017-05-08 16:51:28 +0200

    completed functions

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,63 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  im <- NULL
+  set <- function(y) {
+    x <<- y
+    im <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) im <<- inverse
+  getInverse <- function() im
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
-
 
 ## Write a short comment describing this function
+## The makeCacheMatrix follow the same logic as the vector-based example.
+## The basic variable is "im" that refers to the inverted matrix. 
+
+## Caching the Inverse of a Matrix:
+## Matrix inversion is usually a costly computation and there may be some 
+## benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## Below are a pair of functions that are used to create a special object that 
+## stores a matrix and caches its inverse.
+
+## This function creates a special "matrix" object that can cache its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {
+  im <- NULL
+  set <- function(y) {
+    x <<- y
+    im <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) im <<- inverse
+  getInverse <- function() im
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+## This function computes the inverse of the special "matrix" created by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then it should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  im <- x$getInverse()
+  if (!is.null(im)) {
+    message("getting cached data")
+    return(im)
+  }
+  data <- x$get()
+  im <- solve(data, ...)
+  x$setInverse(im)
+  im
 }
+        ## Return a matrix that is the inverse of 'x'
+## The cacheSolve function follows the same logic as the vector-based example.
